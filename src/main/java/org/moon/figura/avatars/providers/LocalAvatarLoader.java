1643f68658eb5221f5a71eacb21195492ab04533
ENinja33 <83429328+ENinja33@users.noreply.github.com>
2022-04-12 16:47:07 -0400

    Simplify Local Avatar Loader

--- src/main/java/org/moon/figura/avatars/providers/LocalAvatarLoader.java
+++ src/main/java/org/moon/figura/avatars/providers/LocalAvatarLoader.java
@@ -20,77 +20,47 @@ public class LocalAvatarLoader {
 
         //we do not want to accept avatars in the root folder,
         //so we skip right into the children loading
-        allAvatars.getChildren().addAll(loadChildren(allAvatars));
+        allAvatars.fill(true);
     }
 
-    private static List<AvatarPath> loadFromDirectory(AvatarFolder parent) {
-        ArrayList<AvatarPath> avatars = new ArrayList<>();
-        Path path = parent.getPath();
+    public static class AvatarFolder {
 
-        //attempt to load single
-        if (hasAvatar(path))
-            avatars.add(new AvatarPath(path));
-
-        //attempt to load children
-        avatars.addAll(loadChildren(parent));
-
-        return avatars;
-    }
-
-    private static List<AvatarPath> loadChildren(AvatarFolder parent) {
-        ArrayList<AvatarPath> avatars = new ArrayList<>();
-        File[] files = parent.getPath().toFile().listFiles();
-
-        if (files == null) return avatars;
-
-        for (File file : files) {
-            if (!file.isDirectory())
-                continue;
-
-            AvatarFolder avatarPath = new AvatarFolder(file.toPath());
-
-            //load from folder
-            avatarPath.children.addAll(loadFromDirectory(avatarPath));
-
-            //load from nbt
-            //TODO
-
-            //add to return list
-            if (!avatarPath.children.isEmpty())
-                avatars.add(avatarPath);
-        }
-
-        return avatars;
-    }
-
-    private static boolean hasAvatar(Path dir) {
-        dir = dir.resolve("avatar.json");
-        return Files.exists(dir);
-    }
-
-    public static class AvatarPath {
-
+        private final boolean hasAvatar;
+        private final List<AvatarFolder> children = new ArrayList<>();
         private final Path path;
 
-        public AvatarPath(Path path) {
+        public AvatarFolder(Path path) {
             this.path = path;
+            hasAvatar = Files.exists(path.resolve("avatar.json")) || path.toString().endsWith(".moon");
         }
 
+        public List<AvatarFolder> getChildren() {
+            return children;
+        }
         public Path getPath() {
             return path;
         }
-    }
-
-    public static class AvatarFolder extends AvatarPath {
-
-        private final List<AvatarPath> children = new ArrayList<>();
-
-        public AvatarFolder(Path path) {
-            super(path);
+        public boolean hasAvatar() {
+            return hasAvatar;
         }
 
-        public List<AvatarPath> getChildren() {
-            return children;
+        public boolean fill(boolean ignoreAvatars) {
+            if (ignoreAvatars || !hasAvatar) {
+                boolean foundAvatar = false;
+                File[] files = path.toFile().listFiles();
+                if (files != null)
+                    for (File file : files) {
+                        if (file.isDirectory() || file.getName().endsWith(".moon")) {
+                            AvatarFolder folder = new AvatarFolder(file.toPath());
+                            boolean foundAvatarHere = folder.fill(false);
+                            foundAvatar |= foundAvatarHere;
+                            if (foundAvatarHere)
+                                children.add(folder);
+                        }
+                    }
+                return foundAvatar;
+            }
+            return true;
         }
     }
 }
