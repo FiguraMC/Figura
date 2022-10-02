dee533b874b8f2a559bf8505f6bc3ca141d78f9a
Fran <franngoesa@gmail.com>
2022-10-02 01:30:13 -0300

    fixes :D

--- src/main/java/org/moon/figura/animation/AnimationPlayer.java
+++ src/main/java/org/moon/figura/animation/AnimationPlayer.java
@@ -43,11 +43,12 @@ public class AnimationPlayer {
                 if (Float.isNaN(delta))
                     delta = 0;
 
-                FiguraVec3 transform = current.getInterpolation().generate(keyframes, currentIndex, nextIndex, anim.blend, delta);
-                channel.type().apply(part, transform, merge);
+                TransformType type = channel.type();
+                FiguraVec3 transform = current.getInterpolation().generate(keyframes, currentIndex, nextIndex, anim.blend, delta, type);
+                type.apply(part, transform, merge);
 
                 if (anim.override) {
-                    switch (channel.type()) {
+                    switch (type) {
                         case ROTATION -> part.animationOverride |= 1;
                         case POSITION -> part.animationOverride |= 2;
                         case SCALE -> part.animationOverride |= 4;
