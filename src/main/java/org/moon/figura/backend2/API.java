3c0dcc135c39d0a55375a9200a54383282804545
skyrina <116099351+skyrina@users.noreply.github.com>
2022-11-04 19:13:59 +0200

    partially add missing http api endpoints

--- src/main/java/org/moon/figura/backend2/API.java
+++ src/main/java/org/moon/figura/backend2/API.java
@@ -5,6 +5,7 @@ import org.moon.figura.FiguraMod;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.util.UUID;
+import org.moon.figura.avatar.Avatar;
 
 public class API {
 
@@ -32,5 +33,25 @@ public class API {
 
     public HttpRequest getLimits() {
         return header("limits").build();
+    }
+
+    public HttpRequest getMotd() { // can go unused if you dont want this
+        return header("motd").build(); // returns a string which we can set from the figura admin ui and, for example, display it in the figura wardrobe, above the avatar
+    }
+
+    public HttpRequest checkAuth() { // can go unused, i implemented it just in case
+        return header("").build(); // will return 200 OK if token is valid
+    }
+
+    public HttpRequest getAvatar(UUID owner, String id) {
+        return header(owner.toString() + '/' + id).build(); //TODO: store result and apply to player
+    }
+
+    public HttpRequest uploadAvatar(String id, Avatar avatar) {
+        return header(id).PUT().build(); //TODO: needs BodyPublisher in PUT() function that sends the raw nbt data of an avatar, idk how to do it, sorry
+    }
+
+    public HttpRequest deleteAvatar(String id) {
+        return header(id).DELETE().build();
     }
 }
