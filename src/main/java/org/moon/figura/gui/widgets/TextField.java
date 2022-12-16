202148d766d36ac61828ba07640d48f443151c95
Fran <franngoesa@gmail.com>
2022-12-16 16:47:41 -0300

    avatar wizard ui II

--- src/main/java/org/moon/figura/gui/widgets/TextField.java
+++ src/main/java/org/moon/figura/gui/widgets/TextField.java
@@ -110,6 +110,9 @@ public class TextField extends AbstractContainerElement {
 
     @Override
     public void setVisible(boolean visible) {
+        if (visible == isVisible())
+            return;
+
         super.setVisible(visible);
         this.field.setFocus(false);
     }
--- src/main/java/org/moon/figura/wizards/AvatarWizard.java
+++ src/main/java/org/moon/figura/wizards/AvatarWizard.java
@@ -15,45 +15,87 @@ public class AvatarWizard {
         return name != null && !name.isBlank();
     }
 
+    public boolean checkDependency(WizardEntry entry) {
+        if (entry.dependencies == null)
+            return true;
+
+        for (WizardEntry dependency : entry.dependencies) {
+            if (!checkDependency(dependency))
+                return false;
+
+            if (dependency.type == WizardEntry.Type.CATEGORY)
+                continue;
+
+            Object obj = map.get(dependency);
+            if (obj == null || !dependency.validade(obj))
+                return false;
+
+            boolean bl = switch (dependency.type) {
+                case TOGGLE -> (boolean) obj;
+                case TEXT -> !((String) obj).isBlank();
+                default -> true;
+            };
+            if (!bl) return false;
+        }
+
+        return true;
+    }
+
     public void build() {
-        System.out.println("a");
+
     }
 
     public enum WizardEntry {
         //metadata
         Meta(Type.CATEGORY),
         NAME(Type.TEXT),
+        AUTHORS(Type.TEXT, NAME),
         //model stuff
-        Model(Type.CATEGORY),
-        SLIM,
-        CAPE,
-        ELYTRA,
+        Model(Type.CATEGORY, NAME),
+        DUMMY_MODEL(Model),
+        CUSTOM_PLAYER(DUMMY_MODEL),
+        SLIM(CUSTOM_PLAYER),
+        CAPE(DUMMY_MODEL),
+        ELYTRA(DUMMY_MODEL),
         //pivots
-        Pivots(Type.CATEGORY),
-        ITEMS_PIVOT,
-        SPYGLASS_PIVOT,
-        HELMET_ITEM_PIVOT,
-        PARROTS_PIVOT,
+        Pivots(Type.CATEGORY, DUMMY_MODEL),
+        ITEMS_PIVOT(Pivots),
+        SPYGLASS_PIVOT(Pivots),
+        HELMET_ITEM_PIVOT(Pivots),
+        PARROTS_PIVOT(Pivots),
         //scripting
-        Scripting(Type.CATEGORY),
-        HIDE_PLAYER,
-        HIDE_ARMOR,
-        HIDE_CAPE,
-        HIDE_ELYTRA,
-        EMPTY_EVENTS;
+        Scripting(Type.CATEGORY, NAME),
+        DUMMY_SCRIPT(Scripting),
+        HIDE_PLAYER(CUSTOM_PLAYER, DUMMY_SCRIPT),
+        HIDE_ARMOR(DUMMY_SCRIPT),
+        HIDE_CAPE(DUMMY_SCRIPT),
+        HIDE_ELYTRA(DUMMY_SCRIPT),
+        EMPTY_EVENTS(DUMMY_SCRIPT);
 
         private final Type type;
+        private final WizardEntry[] dependencies;
 
         WizardEntry() {
             this(Type.TOGGLE);
         }
-
-        WizardEntry(Type type) {
+        WizardEntry(WizardEntry... dependencies) {
+            this(Type.TOGGLE, dependencies);
+        }
+        WizardEntry(Type type, WizardEntry... dependencies) {
             this.type = type;
+            this.dependencies = dependencies;
         }
 
         public Type getType() {
             return type;
+        }
+
+        public boolean validade(Object object) {
+            return switch (type) {
+                case TOGGLE -> object instanceof Boolean;
+                case TEXT -> object instanceof String;
+                case CATEGORY -> true;
+            };
         }
 
         public enum Type {
