a107b765cd5bea53d76aa20872876b9638887474
Fran <franngoesa@gmail.com>
2023-01-04 17:10:31 -0300

    paperdoll invisible

--- src/main/java/org/moon/figura/mixin/gui/InventoryScreenMixin.java
+++ src/main/java/org/moon/figura/mixin/gui/InventoryScreenMixin.java
@@ -3,6 +3,7 @@ package org.moon.figura.mixin.gui;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.world.entity.LivingEntity;
+import org.moon.figura.avatar.AvatarManager;
 import org.moon.figura.config.Config;
 import org.moon.figura.model.rendering.texture.EntityRenderMode;
 import org.moon.figura.utils.ui.UIHelper;
@@ -16,7 +17,7 @@ public class InventoryScreenMixin {
 
     @Inject(method = "renderEntityInInventory", at = @At("HEAD"), cancellable = true)
     private static void renderEntityInInventory(int x, int y, int size, float mouseX, float mouseY, LivingEntity entity, CallbackInfo ci) {
-        if (!Config.FIGURA_INVENTORY.asBool())
+        if (!Config.FIGURA_INVENTORY.asBool() || AvatarManager.panic)
             return;
 
         UIHelper.drawEntity(x, y, size, mouseX, mouseY, entity, new PoseStack(), EntityRenderMode.MINECRAFT_GUI);
