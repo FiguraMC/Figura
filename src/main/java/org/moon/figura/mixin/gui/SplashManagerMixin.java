b145d9ebff8f4b4ad5e012a2a60a2eaaa4fcbfae
Fran <franngoesa@gmail.com>
2023-01-11 14:40:00 -0300

    splash text
    * changed splash text to include the mark on it

--- src/main/java/org/moon/figura/mixin/gui/SplashManagerMixin.java
+++ src/main/java/org/moon/figura/mixin/gui/SplashManagerMixin.java
@@ -1,8 +1,12 @@
 package org.moon.figura.mixin.gui;
 
 import net.minecraft.client.resources.SplashManager;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import org.moon.figura.FiguraMod;
+import org.moon.figura.avatar.Badges;
 import org.moon.figura.config.Config;
+import org.moon.figura.utils.ColorUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,36 +18,40 @@ import java.util.Date;
 @Mixin(SplashManager.class)
 public class SplashManagerMixin {
 
-    @Inject(at = @At("HEAD"), method = "getSplash", cancellable = true)
-    public void init(CallbackInfoReturnable<String> cir) {
+    @Inject(at = @At("RETURN"), method = "getSplash")
+    private void init(CallbackInfoReturnable<String> cir) {
+        FiguraMod.splashText = null;
         if (!Config.EASTER_EGGS.asBool())
             return;
 
         Calendar calendar = FiguraMod.CALENDAR;
         calendar.setTime(new Date());
 
-        String bday = "Happy birthday ";
+        String who = null;
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         switch (calendar.get(Calendar.MONTH)) {
             case Calendar.JANUARY -> {
-                if (day == 1) cir.setReturnValue(bday + "Foxes!");
+                if (day == 1) who = "Foxes";
             }
             case Calendar.MARCH -> {
                 switch (day) {
-                    case 5 -> cir.setReturnValue(bday + "Limits!");
-                    case 24 -> cir.setReturnValue(bday + FiguraMod.MOD_NAME + "!");
+                    case 5 -> who = "Limits";
+                    case 24 -> who = FiguraMod.MOD_NAME;
                 }
             }
-            case Calendar.APRIL -> {
-                if (day == 1) cir.setReturnValue("LARGECHEESE!");
-            }
             case Calendar.JULY -> {
-                if (day == 4) cir.setReturnValue(bday + "Skylar!");
+                if (day == 4) who = "Skylar";
             }
             case Calendar.SEPTEMBER -> {
-                if (day == 21) cir.setReturnValue(bday + "Fran!");
+                if (day == 21) who = "Fran";
             }
+        }
+
+        if (who != null) {
+            FiguraMod.splashText = Component.literal("Happy birthday " + who + " ")
+                    .append(Badges.System.DEFAULT.badge.copy().withStyle(Style.EMPTY.withFont(Badges.FONT).withColor(ColorUtils.Colors.DEFAULT.hex)))
+                    .append("!");
         }
     }
 }
