cace76c411ce89a680d8dc9f708313d7e9df4feb
Fran <franngoesa@gmail.com>
2023-01-26 07:31:47 -0300

    FPM compat
    * fixed the player model losing their head when the FirstPersonMod is present
    * also fixed a typo with blockstate:getSounds()

--- src/main/java/org/moon/figura/mixin/render/renderers/ItemInHandRendererMixin.java
+++ src/main/java/org/moon/figura/mixin/render/renderers/ItemInHandRendererMixin.java
@@ -6,8 +6,6 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
@@ -18,7 +16,6 @@ import org.moon.figura.avatar.Avatar;
 import org.moon.figura.avatar.AvatarManager;
 import org.moon.figura.ducks.SkullBlockRendererAccessor;
 import org.moon.figura.lua.api.vanilla_model.VanillaModelPart;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -31,31 +28,15 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(ItemInHandRenderer.class)
 public abstract class ItemInHandRendererMixin {
 
-    @Shadow @Final private EntityRenderDispatcher entityRenderDispatcher;
-
     @Shadow protected abstract void renderPlayerArm(PoseStack matrices, MultiBufferSource vertexConsumers, int light, float equipProgress, float swingProgress, HumanoidArm arm);
 
     @Unique private Avatar avatar;
-    @Unique private boolean canRender;
-    @Unique private PlayerRenderer playerRenderer;
 
     @Inject(method = "renderHandsWithItems", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/ItemInHandRenderer$HandRenderSelection;renderMainHand:Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void preRender(float tickDelta, PoseStack matrices, MultiBufferSource.BufferSource vertexConsumers, LocalPlayer player, int light, CallbackInfo ci, float f, InteractionHand interactionHand, float g, @Coerce HandRenderSelectionAccessor handRenderSelection, float h, float i) {
-        playerRenderer = null;
-
-        canRender = handRenderSelection.renderMainHand() || handRenderSelection.renderOffHand();
-        if (!canRender)
-            return;
-
-        avatar = AvatarManager.getAvatarForPlayer(player.getUUID());
-        if (avatar != null)
-            playerRenderer = (PlayerRenderer) this.entityRenderDispatcher.getRenderer(player);
-    }
-
-    @Inject(method = "renderHandsWithItems", at = @At("RETURN"))
-    private void posRender(float tickDelta, PoseStack matrices, MultiBufferSource.BufferSource vertexConsumers, LocalPlayer player, int light, CallbackInfo ci) {
-        if (canRender && avatar != null && avatar.luaRuntime != null)
-            avatar.luaRuntime.vanilla_model.PLAYER.restore(playerRenderer.getModel());
+        avatar = null;
+        if (handRenderSelection.renderMainHand() || handRenderSelection.renderOffHand())
+            avatar = AvatarManager.getAvatarForPlayer(player.getUUID());
     }
 
     @Inject(method = "renderArmWithItem", at = @At("HEAD"), cancellable = true)
