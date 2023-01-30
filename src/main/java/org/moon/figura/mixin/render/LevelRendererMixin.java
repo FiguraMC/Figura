82c758ab649066d257a71aa0a759cbd3e889337f
Fran <franngoesa@gmail.com>
2023-01-30 10:04:27 -0300

    Update LevelRendererMixin.java

--- src/main/java/org/moon/figura/mixin/render/LevelRendererMixin.java
+++ src/main/java/org/moon/figura/mixin/render/LevelRendererMixin.java
@@ -1,6 +1,7 @@
 package org.moon.figura.mixin.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
@@ -9,7 +10,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
-import org.joml.Matrix4f;
 import org.moon.figura.avatar.Avatar;
 import org.moon.figura.avatar.AvatarManager;
 import org.moon.figura.config.Config;
