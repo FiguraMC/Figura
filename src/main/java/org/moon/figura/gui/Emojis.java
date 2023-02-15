5b4df3e3050a47ba9ca92898a7514b6daaa972c4
Fran <franngoesa@gmail.com>
2023-02-15 15:12:54 -0300

    emoji blacklist
    * upcoming blacklist code for blocking certain emojis in nameplate

--- src/main/java/org/moon/figura/gui/Emojis.java
+++ src/main/java/org/moon/figura/gui/Emojis.java
@@ -67,7 +67,7 @@ public class Emojis {
         return ret;
     }
 
-    public static Component convertEmoji(String string, Style style) {
+    private static Component convertEmoji(String string, Style style) {
         //if the string does not contain the delimiter, then return
         if (!string.contains(":"))
             return Component.literal(string).withStyle(style);
@@ -142,5 +142,9 @@ public class Emojis {
         }
 
         return result;
+    }
+
+    public static Component removeBlacklistedEmojis(Component text) {
+        return text; // TextUtils.replaceInText(text, "[Δ]", TextUtils.UNKNOWN, (s, style) -> style.getFont().equals(FONT), Integer.MAX_VALUE);
     }
 }
