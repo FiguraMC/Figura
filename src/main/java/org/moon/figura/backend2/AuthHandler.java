32b41083200e52c3e77b21498e77e30a863686e1
Fran <franngoesa@gmail.com>
2023-02-21 11:10:10 -0300

    new auth
    * completely reworked how the user authentication is done
    * closes #48

--- src/main/java/org/moon/figura/backend2/AuthHandler.java
+++ src/main/java/org/moon/figura/backend2/AuthHandler.java
@@ -1,108 +1,65 @@
 package org.moon.figura.backend2;
 
-import net.minecraft.client.ClientTelemetryManager;
+import com.mojang.authlib.exceptions.AuthenticationUnavailableException;
+import com.mojang.authlib.exceptions.InsufficientPrivilegesException;
+import com.mojang.authlib.exceptions.InvalidCredentialsException;
+import com.mojang.authlib.exceptions.UserBannedException;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientHandshakePacketListenerImpl;
-import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.client.multiplayer.resolver.ServerAddress;
-import net.minecraft.network.Connection;
-import net.minecraft.network.ConnectionProtocol;
+import net.minecraft.client.User;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
-import net.minecraft.network.protocol.login.ClientboundGameProfilePacket;
-import net.minecraft.network.protocol.login.ServerboundHelloPacket;
 import org.moon.figura.FiguraMod;
-import org.moon.figura.config.Config;
 
-import java.net.InetSocketAddress;
-import java.util.Optional;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 public class AuthHandler {
 
-    private static Connection authConnection;
-
-    public static void tick() {
-        //auth ticking
-        if (authConnection != null) {
-            if (authConnection.isConnected())
-                authConnection.tick();
-            else {
-                authConnection.handleDisconnection();
-                authConnection = null;
-            }
-        }
-    }
-
     public static void auth(boolean reAuth) {
         NetworkStuff.async(() -> {
+            if (!reAuth && NetworkStuff.isConnected())
+                return;
+
+            FiguraMod.LOGGER.info("Authenticating with " + FiguraMod.MOD_NAME + " server...");
+            NetworkStuff.backendStatus = 2;
+
+            Minecraft minecraft = Minecraft.getInstance();
+            User user = minecraft.getUser();
             try {
-                if (!reAuth && NetworkStuff.isConnected())
-                    return;
-
-                if (authConnection != null && !authConnection.isConnected())
-                    authConnection.handleDisconnection();
-
-                FiguraMod.LOGGER.info("Authenticating with " + FiguraMod.MOD_NAME + " server...");
-                NetworkStuff.backendStatus = 2;
-
-                Minecraft minecraft = Minecraft.getInstance();
-                ClientTelemetryManager telemetryManager = minecraft.createTelemetryManager();
-
-                ServerAddress authServer = ServerAddress.parseString(Config.SERVER_IP.asString());
-                InetSocketAddress inetSocketAddress = new InetSocketAddress(authServer.getHost(), authServer.getPort());
-                Connection connection = Connection.connectToServer(inetSocketAddress, minecraft.options.useNativeTransport());
-
-                connection.setListener(new ClientHandshakePacketListenerImpl(connection, minecraft, null, (text) -> FiguraMod.LOGGER.info(text.getString())) {
-                    @Override
-                    public void handleGameProfile(ClientboundGameProfilePacket clientboundGameProfilePacket) {
-                        super.handleGameProfile(clientboundGameProfilePacket);
-                        connection.setListener(new ClientPacketListener(minecraft, null, connection, clientboundGameProfilePacket.getGameProfile(), telemetryManager) {
-                            @Override
-                            public void onDisconnect(Component reason) {
-                                telemetryManager.onDisconnect();
-                                String dc = reason.getString();
-
-                                //parse token
-                                String[] split = dc.split("<", 2);
-                                if (split.length < 2) {
-                                    handleDc(dc);
-                                    return;
-                                }
-
-                                split = split[1].split(">", 2);
-                                if (split.length < 2) {
-                                    handleDc(dc);
-                                    return;
-                                }
-
-                                connected(split[0]);
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onDisconnect(Component reason) {
-                        handleDc(reason.getString());
-                    }
-                });
-
-                connection.send(new ClientIntentionPacket(inetSocketAddress.getHostName(), inetSocketAddress.getPort(), ConnectionProtocol.LOGIN));
-                connection.send(new ServerboundHelloPacket(minecraft.getUser().getName(), minecraft.getProfileKeyPairManager().preparePublicKey().join(), Optional.ofNullable(minecraft.getUser().getProfileId())));
-
-                authConnection = connection;
+                String username = user.getName();
+                String serverID = getServerID(username);
+                FiguraMod.debug("Joining \"{}\" on server \"{}\"", username, serverID);
+                minecraft.getMinecraftSessionService().joinServer(user.getGameProfile(), user.getAccessToken(), serverID);
+                NetworkStuff.authSuccess(getToken(username, serverID));
+            //cringe exceptions
+            } catch (AuthenticationUnavailableException e) {
+                NetworkStuff.authFail(Component.translatable("disconnect.loginFailedInfo.serversUnavailable").getString());
+            } catch (InvalidCredentialsException e) {
+                NetworkStuff.authFail(Component.translatable("disconnect.loginFailedInfo.invalidSession").getString());
+            } catch (InsufficientPrivilegesException e) {
+                NetworkStuff.authFail(Component.translatable("disconnect.loginFailedInfo.insufficientPrivileges").getString());
+            } catch (UserBannedException e) {
+                NetworkStuff.authFail(Component.translatable("disconnect.loginFailedInfo.userBanned").getString());
             } catch (Exception e) {
-                handleDc(e.getMessage());
+                NetworkStuff.authFail(e.getMessage());
             }
         });
     }
 
-    private static void handleDc(String reason) {
-        authConnection = null;
-        NetworkStuff.authFail(reason);
+    // requests //
+
+    protected static String request(HttpRequest request) throws Exception {
+        HttpResponse<String> response = NetworkStuff.client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        if (response.statusCode() != 200)
+            throw new Exception(response.body());
+        return response.body();
     }
 
-    private static void connected(String token) {
-        authConnection = null;
-        NetworkStuff.authSuccess(token);
+    private static String getServerID(String username) throws Exception {
+        return request(HttpRequest.newBuilder(HttpAPI.getUri("/auth/id?username=" + username)).build());
+    }
+
+    private static String getToken(String username, String serverID) throws Exception {
+        return request(HttpRequest.newBuilder(HttpAPI.getUri("/auth/verify?username=" + username + "&id=" + serverID)).build());
     }
 }
