2de5bbba0e601576b3c96c1dfa6d2e87f9b5b01c
Fran <franngoesa@gmail.com>
2023-04-04 14:55:14 -0300

    render firstperson arms
    * added a function to force the rendering of first person arms regardless if you're holding an item or not

--- src/main/java/org/moon/figura/mixin/render/renderers/ItemInHandRendererMixin.java
+++ src/main/java/org/moon/figura/mixin/render/renderers/ItemInHandRendererMixin.java
@@ -10,12 +10,14 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.AbstractSkullBlock;
 import org.moon.figura.avatar.Avatar;
 import org.moon.figura.avatar.AvatarManager;
 import org.moon.figura.ducks.SkullBlockRendererAccessor;
 import org.moon.figura.lua.api.vanilla_model.VanillaModelPart;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,17 +25,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemInHandRenderer.class)
 public abstract class ItemInHandRendererMixin {
 
+    @Shadow protected abstract void renderPlayerArm(PoseStack matrices, MultiBufferSource vertexConsumers, int light, float equipProgress, float swingProgress, HumanoidArm arm);
+
+    @Shadow private ItemStack mainHandItem;
+
     @Inject(method = "renderArmWithItem", at = @At("HEAD"), cancellable = true)
     private void renderArmWithItem(AbstractClientPlayer player, float tickDelta, float pitch, InteractionHand hand, float swingProgress, ItemStack item, float equipProgress, PoseStack matrices, MultiBufferSource vertexConsumers, int light, CallbackInfo ci) {
         Avatar avatar;
-        if (item.isEmpty() || (avatar = AvatarManager.getAvatarForPlayer(player.getUUID())) == null || avatar.luaRuntime == null)
+        if (player.isScoping() || (avatar = AvatarManager.getAvatarForPlayer(player.getUUID())) == null || avatar.luaRuntime == null)
             return;
 
-        HumanoidArm arm = hand == InteractionHand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
-        VanillaModelPart part = arm == HumanoidArm.LEFT ? avatar.luaRuntime.vanilla_model.LEFT_ITEM : avatar.luaRuntime.vanilla_model.RIGHT_ITEM;
+        boolean main = hand ==InteractionHand.MAIN_HAND;
+        HumanoidArm arm = main ? player.getMainArm() : player.getMainArm().getOpposite();
+        Boolean armVisible = arm == HumanoidArm.LEFT ? avatar.luaRuntime.renderer.renderLeftArm : avatar.luaRuntime.renderer.renderRightArm;
 
-        if (!part.checkVisible())
+        boolean willRenderItem = !item.isEmpty();
+        boolean willRenderArm = (!willRenderItem && main) || item.is(Items.FILLED_MAP) || (!willRenderItem && this.mainHandItem.is(Items.FILLED_MAP));
+
+        //hide arm
+        if (willRenderArm && !willRenderItem && armVisible != null && !armVisible) {
             ci.cancel();
+            return;
+        }
+        //render arm
+        if (!willRenderArm && !player.isInvisible() && armVisible != null && armVisible) {
+            matrices.pushPose();
+            this.renderPlayerArm(matrices, vertexConsumers, light, equipProgress, swingProgress, arm);
+            matrices.popPose();
+        }
+
+        //hide item
+        VanillaModelPart part = arm == HumanoidArm.LEFT ? avatar.luaRuntime.vanilla_model.LEFT_ITEM : avatar.luaRuntime.vanilla_model.RIGHT_ITEM;
+        if (willRenderItem && !part.checkVisible()) {
+            ci.cancel();
+        }
     }
 
     @Inject(method = "renderItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderStatic(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;ZLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/world/level/Level;III)V"))
