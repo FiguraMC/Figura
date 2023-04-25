71439bb77e3570bfbb1e8bf87d7fae2f4cf46b92
Fran <franngoesa@gmail.com>
2023-04-25 05:43:49 -0300

    sign emojis
    * made signs render emojis

---
+++ src/main/java/org/moon/figura/mixin/render/renderers/SignRendererMixin.java
@@ -0,0 +1,25 @@
+package org.moon.figura.mixin.render.renderers;
+
+import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.FormattedCharSequence;
+import org.moon.figura.config.Configs;
+import org.moon.figura.gui.Emojis;
+import org.moon.figura.utils.TextUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(SignRenderer.class)
+public class SignRendererMixin {
+
+    @ModifyVariable(method = "renderSignText", at = @At("STORE"))
+    private FormattedCharSequence modifyText(FormattedCharSequence charSequence) {
+        if (!Configs.CHAT_EMOJIS.value)
+            return charSequence;
+
+        Component text = TextUtils.charSequenceToText(charSequence);
+        text = Emojis.applyEmojis(text);
+        return text.getVisualOrderText();
+    }
+}
