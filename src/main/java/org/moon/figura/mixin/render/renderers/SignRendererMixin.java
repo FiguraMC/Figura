a0b5bc72ec5c7098bc69d1a6b23177e43462994a
Fran <franngoesa@gmail.com>
2023-05-19 11:13:37 -0300

    sign emojis
    * made emojis on signs apply BEFORE parsing the text length
    * drawback is that to apply sign emoji changes, the world must be restarted

--- src/main/java/org/moon/figura/mixin/render/renderers/SignRendererMixin.java
+++ src/main/java/org/moon/figura/mixin/render/renderers/SignRendererMixin.java
@@ -2,24 +2,18 @@ package org.moon.figura.mixin.render.renderers;
 
 import net.minecraft.client.renderer.blockentity.SignRenderer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.network.chat.FormattedText;
 import org.moon.figura.config.Configs;
 import org.moon.figura.gui.Emojis;
-import org.moon.figura.utils.TextUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(SignRenderer.class)
 public class SignRendererMixin {
 
-    @ModifyVariable(method = "renderSignText", at = @At("STORE"))
-    private FormattedCharSequence modifyText(FormattedCharSequence charSequence) {
-        if (Configs.EMOJIS.value == 0)
-            return charSequence;
-
-        Component text = TextUtils.charSequenceToText(charSequence);
-        text = Emojis.applyEmojis(text);
-        return text.getVisualOrderText();
+    @ModifyArg(method = "method_45799", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Font;split(Lnet/minecraft/network/chat/FormattedText;I)Ljava/util/List;"))
+    private FormattedText modifyText(FormattedText charSequence) {
+        return Configs.EMOJIS.value > 0 && charSequence instanceof Component text ? Emojis.applyEmojis(text) : charSequence;
     }
 }
