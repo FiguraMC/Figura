e1a68e582f63d272ad2699f07f86413bf2644700
Fran <franngoesa@gmail.com>
2023-05-29 06:35:35 -0300

    e
    * added char typed event
    * fixed parsing of weird bbmodels
    * improved performance of rendering nested special parts
    * updated backend related lang

--- src/main/java/org/moon/figura/gui/screens/WardrobeScreen.java
+++ src/main/java/org/moon/figura/gui/screens/WardrobeScreen.java
@@ -28,9 +28,6 @@ import java.util.List;
 
 public class WardrobeScreen extends AbstractPanelScreen {
 
-    private LoadingErrorWidget loadingErrorWidget;
-    private StatusWidget statusWidget;
-    private AvatarInfoWidget avatarInfo;
     private Label panic;
 
     private Button upload, delete;
@@ -96,11 +93,11 @@ public class WardrobeScreen extends AbstractPanelScreen {
         );
         delete.setActive(false);
 
-        statusWidget = new StatusWidget(entity.getX() + entity.getWidth() - 64, 0, 64);
+        StatusWidget statusWidget = new StatusWidget(entity.getX() + entity.getWidth() - 64, 0, 64);
         statusWidget.setY(entity.getY() - statusWidget.getHeight() - 4);
         addRenderableOnly(statusWidget);
 
-        addRenderableOnly(loadingErrorWidget = new LoadingErrorWidget(statusWidget.getX() - 18, statusWidget.getY(), 14));
+        addRenderableOnly(new LoadingErrorWidget(statusWidget.getX() - 18, statusWidget.getY(), 14));
 
         // -- bottom -- //
 
@@ -121,8 +118,6 @@ public class WardrobeScreen extends AbstractPanelScreen {
                             .applyFormat(ChatFormatting.AQUA)
                             .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
                                     FiguraText.of("gui.new_version.tooltip", Component.literal(NetworkStuff.latestVersion.toString()).withStyle(ChatFormatting.GREEN))
-                                            .append("\n")
-                                            .append(FiguraText.of("gui.new_version_download.tooltip"))
                             ))
                             .withClickEvent(new TextUtils.FiguraClickEvent(UIHelper.openURL(FiguraLinkCommand.LINK.MODRINTH.url + "/versions")))
                     );
@@ -172,7 +167,7 @@ public class WardrobeScreen extends AbstractPanelScreen {
         addRenderableWidget(keybinds);
 
         //avatar metadata
-        addRenderableOnly(avatarInfo = new AvatarInfoWidget(this.width - panels - 4, 56, panels, back.getY() - 60));
+        addRenderableOnly(new AvatarInfoWidget(this.width - panels - 4, 56, panels, back.getY() - 60));
 
         //panic warning - always added last, on top
         addRenderableWidget(panic = new Label(
@@ -187,9 +182,6 @@ public class WardrobeScreen extends AbstractPanelScreen {
     public void tick() {
         //children tick
         super.tick();
-        loadingErrorWidget.tick();
-        statusWidget.tick();
-        avatarInfo.tick();
 
         //panic visible
         panic.setVisible(AvatarManager.panic);
