0ce5f3fd09277c4c23d541a02ae1f615f7c74e32
Alice <alice.is.cute@proton.me>
2023-07-04 23:45:03 +0300

    make configs use their category as namespace

--- common/src/main/java/org/figuramc/figura/config/ConfigType.java
+++ common/src/main/java/org/figuramc/figura/config/ConfigType.java
@@ -106,7 +106,7 @@ public abstract class ConfigType<T> {
         public final Category parent;
 
         public ParentedConfig(String name, Category category, T value) {
-            super(name, value);
+            super(category.id + "." + name, value);
             this.parent = category;
 
             category.children.add(this);
@@ -139,7 +139,7 @@ public abstract class ConfigType<T> {
         public EnumConfig(String name, Category category, int defaultValue, int length) {
             super(name, category, defaultValue);
 
-            name = "config." + name;
+            name = "config." + category.id + "." + name;
 
             // generate enum list
             ArrayList<Component> enumList = new ArrayList<>();
