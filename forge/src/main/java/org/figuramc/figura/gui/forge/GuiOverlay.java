3c638c838e1183dd3c107e7fea26beffc74f9fa8
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-07-18 20:04:02 -0600

    Fixed Forge 1.19.4 and broken Show FPS Option

--- forge/src/main/java/org/figuramc/figura/gui/forge/GuiOverlay.java
+++ forge/src/main/java/org/figuramc/figura/gui/forge/GuiOverlay.java
@@ -1,6 +1,6 @@
 package org.figuramc.figura.gui.forge;
 
-import net.minecraft.client.gui.GuiGraphics;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import org.figuramc.figura.avatar.AvatarManager;
@@ -8,8 +8,8 @@ import org.figuramc.figura.gui.FiguraGui;
 
 public class GuiOverlay implements IGuiOverlay {
     @Override
-    public void render(ForgeGui forgeGui, GuiGraphics guiGraphics, float tickDelta, int screenWidth, int screenHeight) {
+    public void render(ForgeGui forgeGui, PoseStack pose, float tickDelta, int screenWidth, int screenHeight) {
         if (!AvatarManager.panic)
-            FiguraGui.renderOverlays(guiGraphics);
+            FiguraGui.renderOverlays(pose);
     }
 }
