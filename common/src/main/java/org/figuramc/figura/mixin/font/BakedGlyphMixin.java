b9e8a1b21965e7e8885ae4ddd48e6b1257c147ba
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2023-08-28 13:21:26 -0600

    Support ImmediatelyFast's new API

--- common/src/main/java/org/figuramc/figura/mixin/font/BakedGlyphMixin.java
+++ common/src/main/java/org/figuramc/figura/mixin/font/BakedGlyphMixin.java
@@ -2,6 +2,7 @@ package org.figuramc.figura.mixin.font;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.gui.font.glyphs.BakedGlyph;
+import org.figuramc.figura.compat.ImmediatelyFastCompat;
 import org.figuramc.figura.ducks.BakedGlyphAccessor;
 import org.figuramc.figura.font.EmojiContainer;
 import org.figuramc.figura.font.EmojiMetadata;
@@ -57,7 +58,7 @@ public abstract class BakedGlyphMixin implements BakedGlyphAccessor {
         float m = italic ? 1.0f - 0.25f * h : 0f;
         float n = italic ? 1.0f - 0.25f * j : 0f;
 
-        final float singleWidth = 8f / figura$getFontWidthIMF();
+        final float singleWidth = 8f / ImmediatelyFastCompat.getFontWidthIMF();
         float shift = singleWidth * figura$metadata.getCurrentFrame();
 
         float u = u0 + shift;
@@ -66,32 +67,5 @@ public abstract class BakedGlyphMixin implements BakedGlyphAccessor {
         vertexConsumer.vertex(matrix, x + figura$metadata.width + n, l, 0.0f).color(red, green, blue, alpha).uv(u + singleWidth, this.v1).uv2(light).endVertex();
         vertexConsumer.vertex(matrix, x + figura$metadata.width + m, k, 0.0f).color(red, green, blue, alpha).uv(u + singleWidth, this.v0).uv2(light).endVertex();
         ci.cancel();
-    }
-
-    @Unique
-    private float figura$getFontWidthIMF() {
-        if (PlatformUtils.isModLoaded("immediatelyfast")) {
-            String modVersion = PlatformUtils.getModVersion("immediatelyfast");
-            if (PlatformUtils.compareVersionTo(modVersion, "1.1.17") >= 0){
-                try {
-                    Class<?> modClass = Class.forName("net.raphimc.immediatelyfast.ImmediatelyFast");
-                    Field configField = modClass.getDeclaredField("runtimeConfig");
-                    Class<?> configClass = Class.forName("net.raphimc.immediatelyfast.feature.core.ImmediatelyFastRuntimeConfig");
-                    Field font_atlas_resizing = configClass.getDeclaredField("font_atlas_resizing");
-                    return font_atlas_resizing.getBoolean(configField.get(null)) ? 2048.0f : 256.0f;
-                } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException ignored) {
-                }
-            } else {
-                try {
-                    Class<?> modClass = Class.forName("net.raphimc.immediatelyfast.ImmediatelyFast");
-                    Field configField = modClass.getDeclaredField("config");
-                    Class<?> configClass = Class.forName("net.raphimc.immediatelyfast.feature.core.ImmediatelyFastConfig");
-                    Field font_atlas_resizing = configClass.getDeclaredField("font_atlas_resizing");
-                    return font_atlas_resizing.getBoolean(configField.get(null)) ? 2048.0f : 256.0f;
-                } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException ignored) {
-                }
-            }
-        }
-        return 256.0f;
     }
 }
