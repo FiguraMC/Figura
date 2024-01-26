e24f29e3350114f6e768fb2cf4aff1f8a395ac1e
applejuiceyy <30875598+applejuiceyy@users.noreply.github.com>
2024-01-26 19:10:23 +0000

    cheesemobile

--- common/src/main/java/org/figuramc/figura/model/rendertasks/EntityTask.java
+++ common/src/main/java/org/figuramc/figura/model/rendertasks/EntityTask.java
@@ -7,6 +7,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.TagParser;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -21,6 +22,7 @@ import org.figuramc.figura.lua.docs.LuaTypeDoc;
 import org.figuramc.figura.math.vector.FiguraVec2;
 import org.figuramc.figura.mixin.LivingEntityAccessor;
 import org.figuramc.figura.model.FiguraModelPart;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 import org.luaj.vm2.LuaError;
 
@@ -85,31 +87,50 @@ public class EntityTask extends RenderTask {
     }
 
     @LuaWhitelist
+    @Contract("_,_->this")
     @LuaMethodDoc(
-            overloads = @LuaMethodOverload(
-                    argumentTypes = String.class,
-                    argumentNames = "nbt"
-            ),
+            overloads = {
+                    @LuaMethodOverload(
+                            argumentTypes = String.class,
+                            argumentNames = "nbt"
+                    ),
+                    @LuaMethodOverload(
+                            argumentTypes = {String.class, String.class},
+                            argumentNames = {"id", "nbt"}
+                    )
+            },
+
             value = "entity_task.set_nbt"
     )
-    public void setNbt(String nbt) {
+    public EntityTask setNbt(String nbtOrId, String nullOrNbt) {
         try {
-            CompoundTag b = (new TagParser(new StringReader(nbt))).readStruct();
-            if (!b.contains("id", CompoundTag.TAG_STRING)) {
-                throw new LuaError("Nbt must contain id");
+            CompoundTag finalNbt;
+            if(nullOrNbt == null) {
+                finalNbt = (new TagParser(new StringReader(nbtOrId))).readStruct();
+
+                if (!finalNbt.contains("id", CompoundTag.TAG_STRING)) {
+                    throw new LuaError("Nbt must contain id");
+                }
             }
+            else {
+                finalNbt = (new TagParser(new StringReader(nullOrNbt))).readStruct();
+                finalNbt.put("id", StringTag.valueOf(nbtOrId));
+            }
+
             assert Minecraft.getInstance().level != null;
-            entity = EntityType.loadEntityRecursive(b, Minecraft.getInstance().level, Function.identity());
+            entity = EntityType.loadEntityRecursive(finalNbt, Minecraft.getInstance().level, Function.identity());
             if (entity == null) {
                 throw new LuaError("Could not create entity");
             }
             ticksSinceEntity = Minecraft.getInstance().level.getGameTime() - entity.tickCount;
         } catch (CommandSyntaxException e) {
-            throw new LuaError(e);
+            throw new LuaError(e.getMessage());
         }
+        return this;
     }
 
     @LuaWhitelist
+    @Contract("_->this")
     @LuaMethodDoc(
             overloads = @LuaMethodOverload(
                     argumentTypes = String.class,
@@ -117,13 +138,15 @@ public class EntityTask extends RenderTask {
             ),
             value = "entity_task.update_walking_distance"
     )
-    public void updateWalkingDistance(float distance) {
+    public EntityTask updateWalkingDistance(float distance) {
         if(entity != null && entity instanceof LivingEntity living) {
             ((LivingEntityAccessor) living).invokeUpdateWalkAnimation(distance);
         }
+        return this;
     }
 
     @LuaWhitelist
+    @Contract("_->this")
     @LuaMethodDoc(
             overloads = @LuaMethodOverload(
                     argumentTypes = FiguraVec2.class,
@@ -131,12 +154,13 @@ public class EntityTask extends RenderTask {
             ),
             value = "entity_task.set_head_rotation"
     )
-    public void setHeadRotation(FiguraVec2 vec2) {
+    public EntityTask setHeadRotation(FiguraVec2 vec2) {
         if(entity != null && entity instanceof LivingEntity living) {
             living.yHeadRot = (float) vec2.y;
             living.yHeadRotO = (float) vec2.y;
             living.setXRot((float) vec2.x);
             living.xRotO = (float) vec2.x;
         }
+        return this;
     }
 }
