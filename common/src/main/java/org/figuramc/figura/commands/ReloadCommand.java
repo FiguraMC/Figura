ed3397e9445e600783083df72c1df63674ca4f9f
Murky <65870445+TannerTheAnimal@users.noreply.github.com>
2025-01-17 09:52:09 -0600

    Update ReloadCommand.java

--- common/src/main/java/org/figuramc/figura/commands/ReloadCommand.java
+++ common/src/main/java/org/figuramc/figura/commands/ReloadCommand.java
@@ -12,7 +12,7 @@ class ReloadCommand {
     public static LiteralArgumentBuilder<FiguraClientCommandSource> getCommand() {
         LiteralArgumentBuilder<FiguraClientCommandSource> cmd = LiteralArgumentBuilder.literal("reload");
         cmd.executes(context -> {
-            AvatarManager.reloadAvatars();
+            AvatarManager.clearAllAvatars();
             FiguraToast.sendToast(FiguraText.of("toast.reload"));
             return 1;
         });
