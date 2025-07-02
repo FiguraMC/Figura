d9d2a0e9d0dfdfdfd86734c46325e7a5b14331fc
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2025-07-02 17:47:23 -0400

    Remove unnecessary throw

--- common/src/main/java/org/figuramc/figura/lua/api/world/WorldAPI.java
+++ common/src/main/java/org/figuramc/figura/lua/api/world/WorldAPI.java
@@ -150,9 +150,6 @@ public class WorldAPI {
                 Math.min(min.getY() + 8, max.getY()),
                 Math.min(min.getZ() + 8, max.getZ())
         );
-        if (min.compareTo(max) > 0) {
-            throw new LuaError("Your max value can't be smaller than your min!");
-        }
         Level world = getCurrentWorld();
         if (!world.hasChunksAt(min, max))
             return list;
