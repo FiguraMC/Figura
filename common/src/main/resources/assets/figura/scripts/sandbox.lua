9558c8db75d9a8fbe2149a4ae4bc6ba851821a3a
xandercreates <69139978+xandercreates@users.noreply.github.com>
2026-08-17 22:24:23 -0700

    Add limit to string.rep function
    Prevent Memory Related Crashes from Stupidly long Repeating Strings.

--- common/src/main/resources/assets/figura/scripts/sandbox.lua
+++ common/src/main/resources/assets/figura/scripts/sandbox.lua
@@ -8,5 +8,13 @@ dofile = nil
 loadfile = nil
 collectgarbage = nil
 
+local _original_rep = string.rep
+string.rep = function(s, n, sep)
+    if n > 10000000 then
+        error("string.rep: too many repetitions (" .. n .. " > 100000) If you're seeing this, Congrats. You can't crash with strings reaching GiBs in length", 3)
+    end
+    return _original_rep(s, n, sep)
+end
+
 -- GS easter egg
 _GS = _G
